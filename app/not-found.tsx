b76a90e85a80c9bbb27cdfa9e886fc470db554d1
hwahyeon <hwahyeon@knou.ac.kr>
2024-02-05 02:41:33 +0900

    feat: Create a navigation

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,10 @@
-export default function NotFound(){
-  return <h1>Not found!</h1>
+import Navigation from "../components/navigation";
+
+export default function NotFound() {
+  return (
+    <div>
+      <Navigation />
+      <h1>Not found!</h1>
+    </div>
+  );
 }
